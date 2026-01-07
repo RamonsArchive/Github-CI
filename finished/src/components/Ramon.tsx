0b49801ce1cbe6b67a0978f0d5dfd44d2e5f8c47
RamonsArchive <rmcdeem.m@gmail.com>
2026-01-07 13:50:34 -0800

    added daniel part two MISTAKE

--- finished/src/components/Ramon.tsx
+++ finished/src/components/Ramon.tsx
@@ -7,7 +7,7 @@ const Ramon = () => {
   return (
     <div className="flex flex-col items-center justify-center px-3 py-2 rounded-xl bg-slate-800 border border-slate-500 ">
       <p className="flex text-sm whitespace-pre-wrap wrap-break-words overflow-wrap-anywhere">
-        Ramon
+        Danielll part 2
       </p>
     </div>
   );
