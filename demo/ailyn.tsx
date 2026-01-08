fedd95ee67b3e05dfb999e2be7f873f46dfa68a6
ailynb006 <pbecerracorvera@ucsd.edu>
2026-01-07 16:35:24 -0800

    correct name tag ailyn

---
+++ demo/ailyn.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const ailyn = () => {
+    return (
+        <div className="flex flex-col items-center justify-center px-3 py-2 rounded-xl bg-pink-500 border border-slate-500">
+            <p className="text-sm text-slate-100">Ailyn</p>
+        </div> );
+};
+
+export default ailyn;
